2b163efe918608945a04c97350c9eff1f8fddaf0
dennisnyamweya <denisnyamweya813@gmail.com>
2019-08-05 14:17:06 +0300

    deployment to heroku

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='123'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:123@localhost/pitchy'
+export MAIL_USERNAME='deno@gmail.com'
+export MAIL_PASSWORD='123'
+
+python3.6 manage.py server
